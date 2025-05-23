fab87de8c2c19a5e22cf0caccfb2a8532b004cb1
abhideepchoubey <101349783+abhideepchoubey@users.noreply.github.com>
2025-05-23 05:55:17 +0000

    datatypes_conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,10 @@
+let score="33abc"
+let valueinnumber=Number(score)
+
+console.log(typeof(score));
+//giving number as type but when we check value we get as NaN not a number
+//for null value converted into 0
+//true->1,false->0
+//""->false
+//"abhi"->true
+//1->true,0->false
