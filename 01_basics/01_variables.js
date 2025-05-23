03dc0072b92404997437e7e44a0f2d3b8309521b
abhideepchoubey <101349783+abhideepchoubey@users.noreply.github.com>
2025-05-23 05:40:21 +0000

    datatypes&variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=1445
+let accountemail="abhi@gmail.com"
+var accountpassword="uyr8w"
+//accountcity="Jaipur"
+//not allowed
+console.log(accountId);
+//if you declare a variable & don't give value then the javascript assumes its value as undefined
+//console.table([]) prints values in tabular format
+/*
+prefer not to use var
+because of issue in block scope & functional scope
+*/
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"user strict";// treat all js code as newer version
+
+//alert(3+3) //we are using nodejs ,not browser
+//code readability is most important in interview
+let age=18
+/*
+number=> 2 to power 53
+bigint
+string
+boolean
+null-standalone value(represantation of empty value)
+undefined-value not assigned
+symbol-for defining uniqueness
+object
+*/
+console.log(typeof null);//object output
+console.log(typeof undefined); //undefined output
+
+
