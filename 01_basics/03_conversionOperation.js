2a5e31cf964d6535d1b13603aabc4db0ce015c51
abhideepchoubey <101349783+abhideepchoubey@users.noreply.github.com>
2025-05-23 06:30:03 +0000

    conversion_operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,24 @@
 let score="33abc"
 let valueinnumber=Number(score)
 
-console.log(typeof(score));
+//onsole.log(typeof(score));
 //giving number as type but when we check value we get as NaN not a number
 //for null value converted into 0
 //true->1,false->0
 //""->false
 //"abhi"->true
 //1->true,0->false
+//***************Operations**************** */
+let value=3
+let negvalue=-value
+
+let str1="hello"
+let str2=" abhi"
+let str3=str1+str2
+
+// console.log("1"+1+1);//111 as output
+// console.log(1+1+"1");//21 as output
+//+true->1 something to number
+let num1,num2,num3
+num1=num2=num3=2+2
+
