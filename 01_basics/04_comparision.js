1ea28bda6ddbd6ef25d201459a2eb905868a0450
abhideepchoubey <101349783+abhideepchoubey@users.noreply.github.com>
2025-05-24 05:12:25 +0000

    comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,5 @@
+console.log("2">1);//true value as output
+console.log(null>=0); //gives true as output 
+//comparisions >= <= convert null into number converting it to zero whereas == does not work like that
+//=== strict check ,checkes data type & value but == does conversion
+
